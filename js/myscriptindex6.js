1706ec4d80b731f733c42dc2628c9a854718b003
tharunkarnekota <Tharunkarnekota@gmail.com>
2021-10-03 23:34:01 +0530

    website practise

---
+++ js/myscriptindex6.js
@@ -0,0 +1,33 @@
+//document.getElementById('demo).innerHtml = 'js is working..'
+//$('#demo').html('jquery is working...);
+
+//document.getElementById('demo').style.color = 'blue';
+//$('#demo').css('color','blue');
+
+//document.getElementById('demo').style.color = 'blue';
+//document.getElementById('demo').style.fontsize = '50px';
+// $('#demo').css({
+//     color:'yellow',
+//     fontSize: '100px'
+// });
+
+//$('#demo').hide(1000);
+
+//$('#demo').hide(1000).show(1000);
+
+// $(document).ready(function(){
+//     $('#demo').hide(1000).show(1000);
+// })
+
+//shortform of document function
+// $(function(){
+//     $('#demo').hide(1000).show(1000);
+// })
+
+
+$(document).ready(function(){
+    $('#demo').hide(1000).show(1000);
+})
+$(window).on("load",function(){
+    alert('welcome')
+})
